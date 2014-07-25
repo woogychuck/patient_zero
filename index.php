fd49089d78d01565768d4488cd59b4d4b04930a0
joe.theriault <joe.theriault@meltwater.com>
2014-07-25 14:18:55 -0400

    Stupid search example

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <h1>Patient Zero Prototype</h1>
 
 <ul>
+    <li><a href="search.php">Search</a><li>
     <li><a href="volume.php">Volume Visualization</a><li>
+    <li><a href="historical.php">Historical Visualization</a><li>
 </ul>
 <?php require_once 'templates/footer.php' ?>
