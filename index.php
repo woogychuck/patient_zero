21cd2849803dc4d42f9dc503c728d591e0ed4aa5
joe.theriault <joe.theriault@meltwater.com>
2014-07-25 14:41:07 -0400

    Pushing the history eraser button

--- index.php
+++ index.php
@@ -5,6 +5,5 @@
 <ul>
     <li><a href="search.php">Search</a></li>
     <li><a href="volume.php">Volume Visualization</a></li>
-    <li><a href="historical.php">Historical Visualization</a></li>
 </ul>
 <?php require_once 'templates/footer.php' ?>
