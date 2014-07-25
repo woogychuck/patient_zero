89d188bef90da29fe04ca86b7582a27d99c4482f
Ian Muir <ian.muir@meltwater.com>
2014-07-25 10:54:46 -0400

    Project structure setup

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require_once 'templates/header.php' ?>
+
+<h1>Patient Zero Prototype</h1>
+
+<ul>
+    <li><a href="historical.php">Historical Visualization</a><li>
+</ul>
+<?php require_once 'templates/footer.php' ?>
--- templates/header.php
+++ templates/header.php
@@ -6,8 +6,10 @@
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script type="text/javascript" src="http://code.jquery.com/ui/1.11.0/jquery-ui.min.js"></script>
     <script type="text/javascript" src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="css/foundation/js/foundation.min.js"></script>
+
+    <link rel="stylesheet" href="css/foundation/css/normalize.css">
+    <link rel="stylesheet" href="css/foundation/css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-
-</body>
-</html>
