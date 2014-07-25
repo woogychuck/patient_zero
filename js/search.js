7b1e5f36398def829351a09f8ff9ef74b058d13a
joe.theriault <joe.theriault@meltwater.com>
2014-07-25 16:18:06 -0400

    Searches with "until" dates

--- js/search.js
+++ js/search.js
@@ -2,8 +2,10 @@
 PatientZero.Search = (function Search ($, config) {
     var exports = {};
 
-    exports.getResults = function getResults (search) {
-        var promise = $.getJSON(config.baseUrl + 'data/search_results_' + search.term + '.json');
+    exports.getResults = function getResults (search, until) {
+        var dateLimit = until ? '_' + until.valueOf() : '';
+
+        var promise = $.getJSON(config.baseUrl + 'data/search_results_' + search.term + dateLimit + '.json');
 
         return promise;
     };
